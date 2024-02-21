36a48748a63211f25d61d71a86f3305878ef3ee8
Rie Amasato <rie_amasato@icloud.com>
2024-02-21 03:18:38 +0000

    定時実行テスト

---
+++ app.py
@@ -0,0 +1,23 @@
+import schedule
+
+import time
+import datetime
+import os
+import requests
+
+def DCsend(message,name):
+	print(name,": " , os.environ[name])
+	discordWH=os.environ[name]
+	data={"content":" "+str(message)+" "}
+	requests.post(discordWH,data=data)
+  
+def job():
+    print("JOB", datetime.datetime.now())
+    DCsend("Job"+str(datetime.datetime.now()), "WHURL")
+
+
+schedule.every().minute.at(":00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
