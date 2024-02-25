2b06a918060b13f6d81058f3a9e949d064d3e6f4
Rie Amasato <51439946+rie-amasato@users.noreply.github.com>
2024-02-25 13:44:32 +0900

    Update server.py
    公式のServer.pyをそのまま使う

--- server.py
+++ server.py
@@ -1,23 +1,5 @@
-import schedule
-import requests
-
-import time
-import datetime
-import os
-
-def DCsend(message,name):
-	print(name,": " , os.environ[name])
-	discordWH=os.environ[name]
-	data={"content":" "+str(message)+" "}
-	requests.post(discordWH,data=data)
-  
-def job():
-    print("JOB", datetime.datetime.now())
-    DCsend("Job"+str(datetime.datetime.now()), "WHURL")
-
-
-schedule.every().minute.at(":00").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+import sys
+from gunicorn.app.wsgiapp import run
+if __name__ == '__main__':
+    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
+    sys.exit(run())
